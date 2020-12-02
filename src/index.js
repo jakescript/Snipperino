e95f89f9427693dc51b171cb51ee24ccfbd7d3bf
Jacob Goldstein <contactjakefilms@gmail.com>
2020-12-02 17:57:39 -0500

    react app started

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("haha")
+import React from "react"
+import ReactDOM from "react-dom"
+const app = document.querySelector("#app");
+
+class App extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            name: "jake"
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>{this.state.name}</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, app)
